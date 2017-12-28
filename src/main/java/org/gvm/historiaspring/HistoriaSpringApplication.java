70fc5d733131897ab6f6b924509577a17595daac
Yudha <agung.y.evianto@gvmnetworks.com>
2017-12-28 16:22:28 +0700

    Add New File

---
+++ src/main/java/org/gvm/historiaspring/HistoriaSpringApplication.java
@@ -0,0 +1,12 @@
+package org.gvm.historiaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoriaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoriaSpringApplication.class, args);
+	}
+}
